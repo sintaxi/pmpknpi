b8c1d2165b1cbd33f5666ba343e6602b885de3a4
brockwhitten <brockwhitten@8b0ec7b7-bd45-0410-8a4e-3563a9e70f90>
2008-03-20 09:03:41 +0000

    merb_can_filter plugin now working with 0.9
    git-svn-id: https://pmpknpi.googlecode.com/svn/trunk@51 8b0ec7b7-bd45-0410-8a4e-3563a9e70f90

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,17 +1,5 @@
 module Merb
   module ArticlesHelper
-    
-    def pluralize(count, singular, plural = nil)
-       "#{count || 0} " + if count == 1 || count == '1'
-        singular
-      elsif plural
-        plural
-      elsif Object.const_defined?("Inflector")
-        Inflector.pluralize(singular)
-      else
-        singular + "s"
-      end
-    end
   
   end
 end
--- lib/active_record_extension.rb
+++ lib/active_record_extension.rb
@@ -1,3 +1,33 @@
+module Merb
+  module Helpers
+    # Monkey Patch!
+    def checkbox_field(attrs = {}, hidden_attrs={})
+      boolbox = true
+      boolbox = false if ( attrs.has_key?(:boolean) and !attrs[:boolean] )
+      attrs.delete(:boolean)
+
+      if( boolbox )
+              on            = attrs.delete(:on)  || 1
+              off           = attrs.delete(:off) || 0
+            # attrs[:value] = on if ( (v = attrs[:value]).nil? || v != "" )
+              attrs[:value] = on if ( (v = attrs[:value]).nil? || v != "1" )
+      else
+              # HTML-escape the value attribute
+              attrs[:value] = escape_xml( attrs[:value] )
+      end
+
+      attrs.merge!(:type => :checkbox)
+      attrs.add_html_class!("checkbox")
+      (boolbox ? hidden_field({:name => attrs[:name], :value => off}.merge(hidden_attrs)) : '') + optional_label(attrs){self_closing_tag("input", attrs)}
+    end
+  end
+end
+
+
+###
+### This is now a Plugin 'sudo gem install merb_can_filter'
+###
+
 # module Sintaxi
 #   module MerbFilter
 #     
