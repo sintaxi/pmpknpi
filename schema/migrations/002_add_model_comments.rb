c2aba7e9ed5ad66ec02121bea3fbe6983cd8ba53
brockwhitten <brockwhitten@8b0ec7b7-bd45-0410-8a4e-3563a9e70f90>
2008-04-07 10:13:40 +0000

    refactored voting system
    git-svn-id: https://pmpknpi.googlecode.com/svn/trunk@62 8b0ec7b7-bd45-0410-8a4e-3563a9e70f90

--- schema/migrations/002_add_model_comments.rb
+++ schema/migrations/002_add_model_comments.rb
@@ -2,13 +2,16 @@ class AddModelComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
       t.column :article_id,         :integer
-      t.column :author,             :string
+      t.column :user_ip,            :string
+      t.column :user_agent,         :string
+      t.column :referrer,           :string
+      t.column :approved,           :boolean,   :default => false
       t.column :name,               :string
       t.column :email,              :string
       t.column :website,            :string
-      t.column :mods_up,            :text,      :default => ""
-      t.column :mods_down,          :text,      :default => ""
-      t.column :mods_count,         :integer,   :default => 1
+      t.column :yay,                :text,      :default => ""
+      t.column :nay,                :text,      :default => ""
+      t.column :vote_count,         :integer,   :default => 1
       t.column :body,               :text
       t.column :body_html,          :text
       t.column :created_at,         :datetime
